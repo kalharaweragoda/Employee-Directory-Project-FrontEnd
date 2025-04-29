639347ea72767718417c545ddb7049711a3f6889
kalharaweragoda <kalharaweragoda@gmail.com>
2025-04-29 14:40:12 +0530

    manage-employee complete

--- emp-app/src/app/page/manage-emp/manage-emp.component.ts
+++ emp-app/src/app/page/manage-emp/manage-emp.component.ts
@@ -1,11 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from '../../navbar/navbar.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-manage-emp',
-  imports: [],
+  imports: [FormsModule,HttpClientModule,CommonModule,NavbarComponent],
   templateUrl: './manage-emp.component.html',
   styleUrl: './manage-emp.component.css'
 })
 export class ManageEmpComponent {
+
+  public employeeObj ={
+    Name:"",
+    Email:"",
+    Department:"",
+    CreatedAt:"",
+    UpdatedAt:""
+  }
+
+  constructor(private http:HttpClient){}
+  
+  addEmployee(){
+    this.http.post("http://localhost:8081/emp-controller/add-employee",this.employeeObj).subscribe(
+      (data) =>{
+        Swal.fire({
+          title: "Employee add!",
+          text: "You clicked the button!",
+          icon: "success"
+        });
+        
+      }
+    )
+  }
+
 
 }
